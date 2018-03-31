fa3003be4bb748457fbcec60dfae00361da71328
Baptiste KIEFFER <baptiste.kief@gmail.com>
2018-03-31 15:34:56 +0200

    tiny mce

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <script src='https://cloud.tinymce.com/stable/tinymce.min.js'></script>
+  <script>
+  tinymce.init({
+    selector: '#mytextarea'
+  });
+  </script>
+</head>
+
+<body>
+<h1>TinyMCE Quick Start Guide</h1>
+  <form action="form.php" method="post">
+    <iframe id="coucou" name="coucou">Hello, World!</iframe>
+    <input type="submit" name="submit">
+  </form>
+
+
+
+
+
+</body>
+</html>
