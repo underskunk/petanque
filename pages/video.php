1596b5e46f2d616a39c942f8be76a0bb7359e174
ThomasBoulange <thomasboulange@gmail.com>
2018-03-31 19:45:46 +0200

    video

--- pages/video.php
+++ pages/video.php
@@ -6,6 +6,9 @@
 		</div>
 		<div class="col-md-10 col-sm-12 col-xs-12">
 			<h1 class="center">Vidéos</h1>
+			<div align="right">
+				<a href="accueil_admin.php"><button type="button" class="btn btn-success" aria-label="Left Align"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></button></a>
+			</div>
 			<div class="panel panel-default">
 				<div class="panel-body">
 					<div class="col-md-6 col-sm-8 col-xs-12">
@@ -13,7 +16,8 @@
 							<iframe class="img-responsive" src="https://www.youtube.com/embed/8XiqrERZo_8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 						</div>
 					</div>
-					<div class="col-md-6 col-sm-4 col-xs-0">
+					<div class="col-md-6 col-sm-4 col-xs-12">
+						<h2>Dernière Vidéo :</h2>
 					</div>
 				</div>
 			</div>
