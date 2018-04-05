ec93429eee15d9174938a536188f61416aa41e11
Baptiste KIEFFER <baptiste.kief@gmail.com>
2018-04-05 17:16:10 +0200

    test bouton rank

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,14 +1,20 @@
 <?php include "../template/header.php" ?>
 <?php include "../template/bdd.php" ?>
+<?php $rank = 1; ?>
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-1 col-sm-0 col-xs-0">
 		</div>
 		<div class="col-md-10 col-sm-12 col-xs-12">
 			<h1 class="center">Accueil</h1>
+			<?php 
+			if ($rank === 1) {
+				echo '
 			<div align="right">
 				<a href="accueil_admin.php"><button type="button" class="btn btn-success" aria-label="Left Align"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></button></a>
-			</div>
+			</div>';
+		} 
+		?>
 			<div class="panel panel-default">
 				<div class="panel-body">
 					<?php echo $init_accueuil; ?>
@@ -19,4 +25,4 @@
 		</div>
 	</div>
 </div>
-<?php include "../template/footer.php" ?> 
+<?php include "../template/footer.php" ?>
