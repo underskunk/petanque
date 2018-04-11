e8b12c730dd7ef068819d762f70472db6dfb6f6c
Baptiste KIEFFER <baptiste.kief@gmail.com>
2018-04-11 18:43:06 +0200

    BDD Complete

--- petanque.sql
+++ petanque.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  sam. 31 mars 2018 à 16:18
--- Version du serveur :  10.1.30-MariaDB
--- Version de PHP :  7.2.1
+-- Généré le :  mer. 11 avr. 2018 à 18:42
+-- Version du serveur :  10.1.26-MariaDB
+-- Version de PHP :  7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -37,7 +37,84 @@ CREATE TABLE `accueil` (
 --
 
 INSERT INTO `accueil` (`main_page`) VALUES
-('<div>hghjgfhfghfghfghfgh</div>');
+('<p><em>Bonjour je test des trucs !</em></p>');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `live`
+--
+
+CREATE TABLE `live` (
+  `lien` longtext
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `live`
+--
+
+INSERT INTO `live` (`lien`) VALUES
+('<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/eSjSozKL_EA\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `partenaires`
+--
+
+CREATE TABLE `partenaires` (
+  `id` int(255) NOT NULL,
+  `nom` varchar(9999) CHARACTER SET utf8 NOT NULL,
+  `logo` text CHARACTER SET utf8 NOT NULL,
+  `lieux` text CHARACTER SET utf8 NOT NULL,
+  `activité` varchar(9999) CHARACTER SET utf8 NOT NULL,
+  `site` text CHARACTER SET utf8 NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `video`
+--
+
+CREATE TABLE `video` (
+  `id` int(11) NOT NULL,
+  `lien` text,
+  `titre` mediumtext NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `partenaires`
+--
+ALTER TABLE `partenaires`
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Index pour la table `video`
+--
+ALTER TABLE `video`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `partenaires`
+--
+ALTER TABLE `partenaires`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `video`
+--
+ALTER TABLE `video`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
