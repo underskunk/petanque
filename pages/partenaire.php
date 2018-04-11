0c72ea20adf34484642924cb73a90083d45d0da6
Baptiste KIEFFER <baptiste.kief@gmail.com>
2018-04-11 18:51:15 +0200

    popup admin partenaire

--- pages/partenaire.php
+++ pages/partenaire.php
@@ -4,7 +4,7 @@
 		<div class="col-md-10 col-md-offset-1 col-sm-12 col-xs-12">
 			<h1 class="center">Partenaires</h1>
 			<div align="right">
-				<a href="accueil_admin.php"><button type="button" class="btn btn-success" aria-label="Left Align"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></button></a>
+				<button type="button" id="admin" class="btn btn-success" aria-label="Left Align"><span class="glyphicon glyphicon-edit" aria-hidden="true"></span></button>
 			</div>
 			<div class="panel panel-default">
 				<div class="panel-body">
@@ -60,4 +60,34 @@
 		</div>
 	</div>
 </div>
+
+
+<div id="popup" class="modal">
+	<!-- Modal content -->
+	<div class="modal-content">
+		<span class="close">&times;</span>
+		<form method="post">
+			<label for="nom_partenaire">Ajoutez le nom : </label>
+			<input type="text" name="nom_partenaire"><br>
+			<label for="logo_partenaire">Ajoutez le logo : </label>
+			<input type="text" name="logo_partenaire"><br>
+			<label for="lieu_partenaire">Ajoutez le lieu : </label>
+			<input type="text" name="lieu_partenaire"><br>
+			<label for="activité_partenaire">Ajoutez l'activité : </label>
+			<input type="text" name="activité_partenaire"><br>
+			<label for="site_partenaire">Ajoutez le site : </label>
+			<input type="text" name="site_partenaire"><br>
+			<input type="submit" name="partenaire">
+		</form>
+		<?php
+		/*if (isset($_POST['video'])) {
+			$lien = "INSERT INTO `video` (`lien`, `titre`, `description`) VALUES ('".$_POST['link_video']."', '".$_POST['titre_video']."', '".$_POST['desc_video']."')";
+			$res_lien = $mysqli->query($lien);
+		}*/
+		?>
+	</div>
+</div>
+
+
+
 <?php include "../template/footer.php" ?>
